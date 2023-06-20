f5e022221003e6e4cfc409ed078e1caaa589bdfe
Kili <kilianschapka@icloud.com>
2023-06-20 08:02:36 +0200

    boden hinzugefügt

---
+++ Datenbank.java
@@ -0,0 +1,19 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Datenbank.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Datenbank
+{
+    
+
+    /**
+     * Konstruktor für Objekte der Klasse Datenbank
+     */
+    public Datenbank()
+    {
+        
+    }
+}
--- VIEW.java
+++ VIEW.java
@@ -22,6 +22,8 @@ public class VIEW extends JFrame implements ActionListener
     private JLabel label2;
     private JLabel titel;
     private JLabel label3;
+    private JLabel label4;
+    private JLabel label5;
     
     // Ergänze das fehlende Referenzattribut
     
@@ -34,19 +36,21 @@ public class VIEW extends JFrame implements ActionListener
     {
         text = new JLabel();
         text.setText("Wie heißt du?");
-        text.setLocation(450,240);
+        text.setLocation(500,350);
         text.setSize (900, 200);       
         text.setFont(text.getFont().deriveFont(46f));
+        text.setForeground(new Color(255, 255, 255));
         
         titel = new JLabel();
         titel.setText("Stickman Fight");
-        titel.setLocation(350,0);
+        titel.setLocation(400,-50);
         titel.setSize(900,400);
         titel.setFont(titel.getFont().deriveFont(70f));
+        titel.setForeground(new Color(255, 255, 255));
         
         button = new JButton();
         button.setText("Enter");
-        button.setLocation(700, 400);
+        button.setLocation(700, 500);
         button.setSize (200, 40);
         button.setEnabled(true);
         button.setFont(button.getFont().deriveFont(40f));
@@ -54,7 +58,7 @@ public class VIEW extends JFrame implements ActionListener
         // Initialisiere und konfiguriere das Eingabefeld
         textfield = new JTextField();
         textfield.setText("");
-        textfield.setLocation(400, 400);
+        textfield.setLocation(400, 500);
         textfield.setSize(275, 40);
         textfield.setEnabled(true);
         textfield.setFont(text.getFont().deriveFont(32));
@@ -62,25 +66,39 @@ public class VIEW extends JFrame implements ActionListener
         
         starten = new JButton();
         starten.setText("Starten");
-        starten.setLocation(490, 550);
-        starten.setSize(225, 100);
+        starten.setLocation(550, 575);
+        starten.setSize(200, 75);
         starten.setEnabled(true);
         starten.setFont(button.getFont().deriveFont(30f));
         starten.setBackground(new Color(255, 0, 0));
-        //starten.setBorder(_border_)
+        //starten.set
         
-        ImageIcon stickman1 = new ImageIcon("Stickman1.png");
+        ImageIcon stickman1 = new ImageIcon("Stickman1.1.png");
         
         label2 = new JLabel(stickman1);
         label2.setLocation(675, 100);
         label2.setSize(900, 800);
         
         
-        ImageIcon stickman2 = new ImageIcon("Stickman2.png");
+        ImageIcon stickman2 = new ImageIcon("Stickman2.1.png");
         
         label3 = new JLabel(stickman2);
         label3.setLocation(-250, 100);
         label3.setSize(900, 800);
+        
+        
+        ImageIcon hintergrund = new ImageIcon("Hintergrund2.png");
+        
+        label4 = new JLabel(hintergrund);
+        label4.setLocation(0, 0);
+        label4.setSize(1290, 750);
+        
+        
+        ImageIcon boden = new ImageIcon("Boden.png");
+        
+        label5 = new JLabel(boden);
+        label5.setLocation(0, 500);
+        label5.setSize(1290, 250);
         
         // Hefte den AktionListener an den Button an
         button.addActionListener(this);
@@ -94,6 +112,8 @@ public class VIEW extends JFrame implements ActionListener
         super.add(label2);
         super.add(titel);
         super.add(label3);
+        super.add(label4);
+        super.add(label5);
         //Füge auch die dritte Komponente in das Fenster ein
 
        
@@ -102,21 +122,23 @@ public class VIEW extends JFrame implements ActionListener
         super.setLayout(null);
         super.setSize(1290, 750);
         super.setVisible(true);
-
     }
 
     /**
      * Die Komponenten für Version 1 werden in das Fenster eingefügt.
      */
     public void FensterAufbauenV1() {
-
+        label5.setVisible(true);
         text.setVisible(true);
         button.setVisible(true);
         //Das Eingabefeld muss auf sichtbar gestellt werden
         textfield.setVisible(true);
         starten.setVisible(true);
+        label2.setVisible(true);
         titel.setVisible(true);
         label3.setVisible(true);
+        label4.setVisible(true);
+        label5.setVisible(true);
     }
     /**
      * Die Komponenten für Version 2 werden in das Fenster eingefügt.
@@ -129,8 +151,8 @@ public class VIEW extends JFrame implements ActionListener
         label2.setVisible(false);
         titel.setVisible(false);
         label3.setVisible(false);
-        
-        
+        label4.setVisible(false);
+        label5.setVisible(true);
     }
     
     public void actionPerformed (ActionEvent e){
