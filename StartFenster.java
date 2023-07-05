b11ed7ad715b63f482ebdbda09921b142254b0a5
Berrrryyyy <andreas.herman669@gmail.com>
2023-07-05 20:08:38 +0200

    z

---
+++ StartFenster.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class StartFenster here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StartFenster
+{
+    // instance variables - replace the example below with your own
+    private Daten myDaten;
+
+    /**
+     * Constructor for objects of class StartFenster
+     */
+    public StartFenster()
+    {
+        myDaten = new Daten();
+        myDaten.AllesAuslesen();
+    }
+
+    
+}
